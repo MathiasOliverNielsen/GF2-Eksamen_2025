48e6cff5e0008a6ff017224c3b57f00b5fb23018
MathiasOliverNielsen <math324e@elev.techcollege.dk>
2025-06-24 21:49:08 +0200

    Refactor CSS and HTML structure for improved layout and styling; enhance JavaScript functionality with debugging logs.

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,15 +7,9 @@ function plusSlides(n) {
   showSlides((slideIndex += n));
 }
 
-// Thumbnail image controls
-function currentSlide(n) {
-  showSlides((slideIndex = n));
-}
-
 function showSlides(n) {
   let i;
   let slides = document.getElementsByClassName('mySlides');
-  let dots = document.getElementsByClassName('dot');
   if (n > slides.length) {
     slideIndex = 1;
   }
@@ -25,11 +19,7 @@ function showSlides(n) {
   for (i = 0; i < slides.length; i++) {
     slides[i].style.display = 'none';
   }
-  for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(' active', '');
-  }
   slides[slideIndex - 1].style.display = 'block';
-  dots[slideIndex - 1].className += ' active';
 }
 
 // FORM VALIDATION
@@ -40,6 +30,7 @@ const dialog = document.getElementById('myDialog');
 const signBtn = document.getElementById('sign-up');
 
 function openDialog() {
+  console.log('openDialog called');
   dialog.showModal();
 }
 
